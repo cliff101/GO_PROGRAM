798999f6085630d6126d0d33156ed12504c7650f
cliff101 <44256679+cliff101@users.noreply.github.com>
2020-12-16 12:30:12 +0800

    Create Websocket_server.go

---
+++ Websocket_server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	// Upgrader specifies parameters for upgrading an HTTP connection to a
+	// WebSocket connection.
+	upgrader := &websocket.Upgrader{}
+
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		c, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println("upgrade:", err)
+			return
+		}
+		defer func() {
+			log.Println("disconnect !!")
+			c.Close()
+		}()
+		for {
+			mtype, msg, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				break
+			}
+			log.Printf("receive: %s\n", msg)
+			err = c.WriteMessage(mtype, msg)
+			if err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+	})
+	log.Println("server start at :8899")
+	//ListenAndServe starts an HTTP server with a given address and handler.
+	//The handler is usually nil, which means to use DefaultServeMux. Handle and HandleFunc add handlers to DefaultServeMux:
+	log.Fatal(http.ListenAndServe(":8899", nil))
+}
